19d1139fcd862ab7770211ead1dd1e6768f9275e
Djordje995 <sigma01@protonmail.com>
2019-03-12 17:09:25 -0400

    config update

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or '@jhk/48-T'
+    SECRET_KEY = os.environ.get('99393299335607')
 
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
     'sqlite:///' + os.path.join(basedir, 'app.db')
